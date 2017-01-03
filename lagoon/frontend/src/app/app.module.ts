46d9ad8a3f255780c60f0fd60d09cf4d293e1feb
Evandro Machado <evandrolmachado@hotmail.com>
2017-01-03 19:21:21 -0200

    Re-created repository to count contributions, 2nd day didn't work properly.

---
+++ lagoon/frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
+import { PhotoList } from './components/photo-list.component';
+import { SidePanel } from './components/side-panel.component';
+import { NavBar } from './components/nav-bar.component';
+
+
+import { PhotoService } from './services/photo.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PhotoList,
+    SidePanel,
+    NavBar
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [PhotoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
