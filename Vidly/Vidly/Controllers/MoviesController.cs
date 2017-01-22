4d698a9a5fa83a875cad341ee7a371bfc91f274d
evandro-machado <evandrolmass@gmail.com>
2017-01-22 20:02:20 -0200

    Learned how attribute routing works.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -20,26 +20,11 @@ namespace Vidly.Controllers
 
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        public ActionResult ByReleaseDate(int year, int month)
+        [Route("movies/released/{year(\\d{4})}/{month:regex(\\d{2}:range(1,12))}")]
+        public ActionResult ByReleaseYear(int year, int month)
         {
             return Content(year + "/" + month);
         }
+
     }
 }
