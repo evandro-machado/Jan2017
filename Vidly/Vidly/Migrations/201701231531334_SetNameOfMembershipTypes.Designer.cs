4a356fb61eca20d3d32e3b31b23a0775e4aefdfe
evandro-machado <evandrolmass@gmail.com>
2017-01-23 17:57:26 -0200

    Added migration files, Movies and Customer Details files.

--- Vidly/Vidly/Migrations/201701231531334_SetNameOfMembershipTypes.Designer.cs
+++ Vidly/Vidly/Migrations/201701231531334_SetNameOfMembershipTypes.Designer.cs
@@ -7,13 +7,13 @@ namespace Vidly.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class PopulateMembershipTypes1 : IMigrationMetadata
+    public sealed partial class SetNameOfMembershipTypes : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(PopulateMembershipTypes1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(SetNameOfMembershipTypes));
         
         string IMigrationMetadata.Id
         {
-            get { return "201701231445071_PopulateMembershipTypes1"; }
+            get { return "201701231531334_SetNameOfMembershipTypes"; }
         }
         
         string IMigrationMetadata.Source
