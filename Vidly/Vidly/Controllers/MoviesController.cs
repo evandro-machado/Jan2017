fad3fece01a021f6b66a706a2d94b6a58e84e236
evandro-machado <evandrolmass@gmail.com>
2017-01-23 12:48:53 -0200

    Learned how to create database using code-first migrations.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -8,6 +8,22 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Matrix"}
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -27,38 +43,5 @@ namespace Vidly.Controllers
             return View(viewModel);
 
         }
-
-        public ActionResult Movies()
-        {
-            //var movies = new List<Movie>
-            //{
-            //    new Movie {Id = 1, Name = "Shrek"},
-            //    new Movie {Id = 2, Name = "Matrix"}
-            //};
-
-            return View();
-        }
-
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
     }
 }
