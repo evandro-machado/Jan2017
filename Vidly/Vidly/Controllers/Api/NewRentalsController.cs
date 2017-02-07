62527ed9f52245cb77f14b8f0e0f8339b3f20ff0
evandro-machado <evandrolmass@gmail.com>
2017-02-06 23:27:18 -0200

    Implementing Rental API. Using defensive approach.

---
+++ Vidly/Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals (NewRentalDto newRental)
+        {
+            Customer customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+
+            if (customer == null)
+            {
+                return BadRequest("CustomerId is not valid.");
+            }
+
+            if (newRental.MovieIds.Count == 0)
+            {
+                return BadRequest("No Movie Ids have been given.");
+            }
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+            {
+                return BadRequest("One or more MovieIds are invalid.");
+            }
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available");
+                }
+
+                var rental = new Rental()
+                {
+                    DateRented = DateTime.Now,
+                    Customer = customer,
+                    Movie = movie
+                };
+                if (movie.NumberAvailable > 0)
+                {
+                    movie.NumberAvailable--;
+                    _context.Rentals.Add(rental);
+                }
+            }
+
+            _context.SaveChanges();
+
+
+            return Ok();
+        }
+    }
+}
